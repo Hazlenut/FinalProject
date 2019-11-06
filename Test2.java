b85932f5c4cee12e4ca224bf082c645b99bf35ed
bnschool <bneel@masonlive.gmu.edu>
2019-11-06 16:23:40 -0500

    Test 2

--- Test2.java
+++ Test2.java
@@ -1,8 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class Test2 {
    
    public static void main(String[] arg) {
       
-      System.out.println("Hello");
+      JFrame frame = new JFrame("JFrame");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+      frame.setPreferredSize(new Dimension((int)(.5*screen.getWidth()), (int)(.7*screen.getHeight())));
+      frame.pack();
+      frame.setLocationRelativeTo(null);
+      frame.setVisible(true);
       
    }
    
