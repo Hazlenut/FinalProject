5b8ee3812aea1297c2aca467e8ab4088cc4c74bd
Hazlenut <willie.wung@gmail.com>
2019-11-06 16:20:36 -0500

    testing conflict

--- Test2.java
+++ Test2.java
@@ -1,9 +1,16 @@
+
+
 public class Test2 {
    
    public static void main(String[] arg) {
       
-      System.out.println("Hello");
+      System.out.print("hElo");
+      System.out.println("World");
       
+      
+   }
+   public boolean addable(int a, int b) {
+	   return true;
    }
    
 }
