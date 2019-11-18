54f02d4aa4451cc85d4d5f8557189e0e52c8156d
Hazlenut <willie.wung@gmail.com>
2019-11-18 02:30:08 -0500

    added google search for movies

--- Events.java
+++ Events.java
@@ -28,5 +28,31 @@ public class Events {
 		return list;
 	}
 	
+	public static ArrayList<Movie> getMovies(int zipCode, int numShows) throws IOException {
+		ArrayList<Movie> list = new ArrayList<Movie>();
+		String baseURL = "https://www.google.com/search";
+		String fullURL = baseURL + "?q=movies near " + zipCode;
+		String html = Jsoup.connect(fullURL).get().html();
+		Document document = Jsoup.parse(html);
+		Elements div = document.select("div.kltat span");
+		for(int i = 0; i < numShows; i++) {
+			list.add(new Movie(div.get(i).ownText()));
+		}
+		
+		
+		return list;
+	}
+	public static void main(String[] args) {
+		ArrayList<Movie> test = new ArrayList<Movie>();
+		try {
+			test = getMovies(20166, 5);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(int i = 0; i < test.size(); i++) {
+			System.out.println(test.get(i).getShowName());
+		}
+	}
 	
 }
