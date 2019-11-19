532360cb50219d451af80941dc3b42df513b1113
dnorman6 <57462914+dnorman6@users.noreply.github.com>
2019-11-19 10:44:33 -0500

    test

--- Test.java
+++ Test.java
@@ -1,22 +1,18 @@
 
-<<<<<<< HEAD
-public class Test 
-{
-
-=======
 public class Test {
-	public static void main(String[] args) {
-		
-	int a;
-	int b;
-	int c;
-	String d,e,f,g;
-	System.out.println();
+ public static void main(String[] args) {
+  
+ int a;
+ int b;
+ int c;
+ String d,e,f,g;
+ System.out.println();
    
    System.out.println();
    System.out.println();
    System.out.println();
    
 }
->>>>>>> 01bebdc9a1c4f250aa4e2a5c6eb8997556062dad
+
 }
+
