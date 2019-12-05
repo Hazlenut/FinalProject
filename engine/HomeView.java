dba27c128b6a5943efdd02d260153663d8fece34
Hazlenut <willie.wung@gmail.com>
2019-12-04 23:10:17 -0500

    some home screen details

--- engine/HomeView.java
+++ engine/HomeView.java
@@ -1,8 +1,16 @@
 package engine;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 
 public class HomeView extends View {
 
@@ -13,12 +21,16 @@ public class HomeView extends View {
     public HomeView(String name, Screen screen) {
 
         super(name, screen);
-
+        
         boolean usersExist = User.userFileExists();
         String text = usersExist ? "Welcome to Event Organizer - please enter your User ID" : "Welcome to Event Organizer";
         label = new Label(text);
+        label.setStyle("-fx-font: 24 arial;");
+        label.setWrapText(true);
+        label.setTextAlignment(TextAlignment.CENTER);
         VBox vBox = new VBox();
         textField = new TextField();
+        textField.setPrefWidth(500);
         text = usersExist ? "Enter" : "Continue";
         enterButton = new Button(text);
         enterButton.setOnAction(event -> {
@@ -37,9 +49,14 @@ public class HomeView extends View {
         if(usersExist) {
             vBox.getChildren().addAll(textField, newUserButton);
         }
+        vBox.setSpacing(25);
         vBox.getChildren().add(enterButton);
         Main.setNewUser(!usersExist);
+        vBox.setAlignment(Pos.CENTER);
         getBorderPane().setCenter(vBox);
+        getBorderPane().setBackground(new Background(new BackgroundFill(Color.LAVENDERBLUSH, CornerRadii.EMPTY, Insets.EMPTY)));
+		
+        //BorderPane.setAlignment(vBox, Pos.CENTER);
 
     }
 
